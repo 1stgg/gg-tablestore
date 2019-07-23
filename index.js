a19ba1c83ea9ab108c26c59e2234456421d9dd35
gg_1st <1274405567@qq.com>
2019-07-23 10:28:25 +0800

    gg

--- index.js
+++ index.js
@@ -1,23 +1,28 @@
 let TableStore = require('tablestore')
-const NodeCache = require( "node-cache" );
+// const NodeCache = require( "node-cache" );
 global.db = require('./db.js')
 global.gg = require('./gg_tool.js')
 global.client = 0
-global.getClient = function (argument) {
+
+module.exports = function function_name(account) {
+	global.getClient = function (argument) {
 	if(client){
 		return client
 	}
-	var client = new TableStore.Client(require('./account.js'));
-	global.client = client
-	return client
-}
-global.S = function (key,value) {
-	if(value === undefine){
-		NodeCache.get(key)
-	}else{
-		NodeCache.set(key,value)
+		var client = new TableStore.Client(account);
+		global.client = client
+		return client
 	}
+	return db;
 }
+return
+// global.S = function (key,value) {
+// 	if(value === undefine){
+// 		NodeCache.get(key)
+// 	}else{
+// 		NodeCache.set(key,value)
+// 	}
+// }
 var arg = process.argv.splice(2);
 console.log(13,arg)
 let test = require('./test/index.js')
